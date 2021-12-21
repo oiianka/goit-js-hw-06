a83d0403d5bca2b306e41827ab89783bae5311ea
oiianka <dez-ire@ukr.net>
2021-12-21 23:50:27 +0200

    Finished task 9
    Task 9 is ready

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// console.log(getRandomHexColor());
+const button = document.querySelector('button.change-color')
+const span = document.querySelector('span.color')
+
+button.addEventListener('click', () => {
+  // console.log(getRandomHexColor());
+  span.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
